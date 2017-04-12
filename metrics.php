6d816c1d3fcda675d61486dbefa31b989c48c848
Wojciech Frącz <fraczwojciech@gmail.com>
2017-04-12 23:55:03 +0200

    Metrics for PHP

--- metrics.php
+++ metrics.php
@@ -1,7 +1,19 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Wojciech
- * Date: 2017-04-12
- * Time: 23:05
- */
+$project = 'symfony';
+$command = 'sh -c "./vendor/bin/phpmetrics --report-json=REPORT.json PATH"';
+
+require 'vendor/autoload.php';
+
+$path = './results/' . $project;
+
+$commits = $files = array_diff(scandir($path), ['.', '..', 'README.txt']);
+
+$progress = new \ProgressBar\Manager(0, count($commits));
+
+foreach ($commits as $commit) {
+    $progress->advance();
+    $commandForCommit = str_replace(['REPORT', 'PATH'], [$path . "/$commit/metrics.after", $path . "/$commit/after"], $command);
+    exec($commandForCommit);
+    $commandForCommit = str_replace(['REPORT', 'PATH'], [$path . "/$commit/metrics.before", $path . "/$commit/before"], $command);
+    exec($commandForCommit);
+}
