d39f12bd862ac96e9fd1977cb13fa32d0aad63a4
Wojciech Frącz <fraczwojciech@gmail.com>
2017-04-12 23:05:03 +0200

    Put results in after & before directories instead of suffixes

--- extractor.php
+++ extractor.php
@@ -1,7 +1,7 @@
 <?php
 require 'vendor/autoload.php';
 
-const REPO_DIR = 'C:/Users/Wojciech/Desktop/hibernate-orm';
+const REPO_DIR = 'C:/Users/Wojciech/Desktop/symfony';
 const REFACTOR_KEYWORDS = ['refactor', 'improve', 'reorganize', 'readability'];
 const EXTENSION_FILTER = ['java', 'js', 'ts', 'php', 'cs'];
 
@@ -22,32 +22,41 @@ $outputDir = __DIR__ . '/results/' . basename(REPO_DIR);
 @mkdir($outputDir);
 
 $files = 0;
+$commitsWithFiles = 0;
 
 foreach ($commits as $hash) {
     $progress->advance();
     $output = $outputDir . '/' . $hash;
-    @mkdir($output);
-    $fullCommitMessage = implode(PHP_EOL, executeInRepo('git show -q ' . $hash));
-    file_put_contents($output . '/README.txt', $fullCommitMessage);
+    $outputBefore = $output . '/before';
+    $outputAfter = $output . '/after';
     $changedFiles = executeInRepo('git diff-tree --no-commit-id --name-only -r ' . $hash);
+    $filesInThisCommit = 0;
     foreach ($changedFiles as $changedFile) {
         $filename = basename($changedFile);
         $extension = strtolower(@end(explode('.', $filename)));
         if (in_array($extension, EXTENSION_FILTER)) {
+            @mkdir($output);
+            @mkdir($outputBefore);
+            @mkdir($outputAfter);
             $cmd = "git show {$hash}^:$changedFile";
             $beforeRefactoring = implode(PHP_EOL, executeInRepo("git show {$hash}~1:$changedFile"));
             $afterRefactoring = implode(PHP_EOL, executeInRepo("git show $hash:$changedFile"));
-            $filename = substr($filename, 0, -strlen($extension) - 1);
-            file_put_contents($output . "/$filename.before.$extension", $beforeRefactoring);
-            file_put_contents($output . "/$filename.after.$extension", $afterRefactoring);
+            file_put_contents($outputBefore . "/$filename", $beforeRefactoring);
+            file_put_contents($outputAfter . "/$filename", $afterRefactoring);
             ++$files;
+            ++$filesInThisCommit;
         }
+    }
+    if ($filesInThisCommit > 0) {
+        ++$commitsWithFiles;
+        $fullCommitMessage = implode(PHP_EOL, executeInRepo('git show -q ' . $hash));
+        file_put_contents($output . '/README.txt', $fullCommitMessage);
     }
 }
 
-file_put_contents($outputDir . '/README.txt', 'Commits: ' . count($commits) . PHP_EOL . 'Files: ' . $files);
+file_put_contents($outputDir . '/README.txt', 'Commits: ' . $commitsWithFiles . PHP_EOL . 'Files: ' . $files);
 
-echo "Found $files files with refactor changes";
+echo "Found $files files with refactor changes in $commitsWithFiles commits.";
 
 function executeInRepo($cmd)
 {
