494151fb15b73fcabb089fdec61df16d375e7997
Wojciech Frącz <fraczwojciech@gmail.com>
2017-08-25 01:32:28 +0200

    Generating AST diffs

--- php-ast.php
+++ php-ast.php
@@ -1,7 +1,8 @@
 <?php
 require 'vendor/autoload.php';
 
-const INDENT_OUTPUT = false;
+const INDENT_OUTPUT = true;
+const DIFF_SEPARATOR = '||||||||';
 
 $projectsDir = __DIR__ . '/results/';
 $projects = array_diff(scandir($projectsDir), ['.', '..', 'README.txt']);
@@ -22,21 +23,35 @@ foreach ($projects as $project) {
     $changes = array_diff(scandir($projectDir), ['.', '..', 'README.txt']);
     foreach ($changes as $commitId) {
         $changeDir = $projectDir . '/' . $commitId;
-        $methods = ['before' => [], 'after' => []];
-        foreach (['before', 'after'] as $part) {
-            $files = array_diff(scandir($changeDir . '/' . $part), ['.', '..']);
-            @mkdir($changeDir . '/' . $part . '-ast');
-            foreach ($files as $file) {
-                $tree = ast_dump(ast\parse_file($changeDir . "/$part/$file", $version = 50), 0, $methods[$part]);
-                echo $tree;
-                file_put_contents($changeDir . "/$part-ast/$file.txt", $tree);
+        $filesBefore = array_diff(scandir($changeDir . '/before'), ['.', '..']);
+        @mkdir($changeDir . '/diffs');
+        foreach ($filesBefore as $file) {
+            if (file_exists($changeDir . "/after/$file")) {
+                $methods = ['before' => [], 'after' => [], 'beforeLines' => [], 'afterLines' => []];
+                try {
+                    $treeBefore = ast_dump(ast\parse_file($changeDir . "/before/$file", $version = 50), 0, $methods['before'], $methods['beforeLines']);
+                    $treeAfter = ast_dump(ast\parse_file($changeDir . "/after/$file", $version = 50), 0, $methods['after'], $methods['afterLines']);
+                } catch (Error $e) {
+                    continue;
+                }
+                if (count($methods['before']) || count($methods['after'])) {
+                    $deletedMethods = array_diff_key($methods['before'], $methods['after']);
+                    $addedMethods = array_diff_key($methods['after'], $methods['before']);
+                    $changedMethods = array_diff_assoc($methods['before'], $methods['after']);
+                    $changedMethods = array_diff_key($changedMethods, $deletedMethods);
+                    $changedMethods = array_diff_key($changedMethods, $addedMethods);
+                    foreach ($changedMethods as $changedMethodName => $changedMethodAst) {
+                        $diff = implode(DIFF_SEPARATOR, [
+                            implode('', array_slice(file($changeDir . "/before/$file"), $methods['beforeLines'][$changedMethodName][0] - 1, $methods['beforeLines'][$changedMethodName][1] - $methods['beforeLines'][$changedMethodName][0])),
+                            implode('', array_slice(file($changeDir . "/after/$file"), $methods['afterLines'][$changedMethodName][0] - 1, $methods['afterLines'][$changedMethodName][1] - $methods['afterLines'][$changedMethodName][0])),
+                            $methods['before'][$changedMethodName],
+                            $methods['after'][$changedMethodName],
+                        ]);
+                        file_put_contents($changeDir . '/diffs/' . $file . $changedMethodName . '.txt', $diff);
+                    }
+                }
             }
-        }
-        if (count($methods['before']) || count($methods['after'])) {
-            $changedMethods = array_diff_assoc($methods['before'], $methods['after']);
-            $deletedMethods = array_diff_key($methods['before'], $methods['after']);
-            $addedMethods = array_diff_key($methods['after'], $methods['before']);
-            var_dump($methods);
+//                file_put_contents($changeDir . "/$part-ast/$file.txt", $tree);
         }
     }
     $progress->advance();
@@ -47,7 +62,7 @@ foreach ($projects as $project) {
 
 
 /** Dumps abstract syntax tree */
-function ast_dump($ast, int $options = 0, &$methods = []): string
+function ast_dump($ast, int $options = 0, &$methods = [], &$methodLines = [], $nextChildLine = 1): string
 {
     if ($ast instanceof ast\Node) {
         $result = ast\get_kind_name($ast->kind);
@@ -71,8 +86,15 @@ function ast_dump($ast, int $options = 0, &$methods = []): string
         $isMethod = false;
         if ($result == 'AST_METHOD') {
             $isMethod = true;
+            $methodStartsAtLine = $ast->lineno;
+            if (is_array($ast->children['stmts']) && is_object(end($ast->children['stmts']->children))) {
+                $methodEndsAtLine = end($ast->children['stmts']->children)->lineno + 1;
+            } else {
+                $methodEndsAtLine = $methodStartsAtLine + 2; // empty method
+            }
             $methodName = $ast->children['name'];
             $hasDocComment = isset($ast->children['docComment']) && $ast->children['docComment'];
+            $result = '';
             $result .= $hasDocComment ? 'HAS_DOC_COMMENT' : 'NO_DOC_COMMENT';
             $declaresReturnType = !!$ast->children['returnType'];
             $result .= $declaresReturnType ? 'HAS_RETURN_TYPE' : 'NO_RETURN_TYPE';
@@ -85,7 +107,7 @@ function ast_dump($ast, int $options = 0, &$methods = []): string
                         $param->children['default'] ? 'PARAM_DEFAULT' : 'NO_PARAM_DEFAULT',
                     ]);
                 }, $ast->children)) . (count($ast->children) ? $paramDelimiter : '');
-        } else if ($result == 'AST_STMT_LIST') {
+        } else if (in_array($result, ['AST_STMT_LIST', 'AST_NAME', 'AST_ARG_LIST'])) {
             $result = '';
         }
 
@@ -97,14 +119,23 @@ function ast_dump($ast, int $options = 0, &$methods = []): string
         unset($ast->children['__declId']);
 
         foreach ($ast->children as $i => $child) {
-            if (INDENT_OUTPUT) {
-                $result .= "\n";
+            $nextMyChildLine = PHP_INT_MAX;
+            if (is_int($i) && isset($ast->children[$i + 1]) && is_object($ast->children[$i + 1])) {
+                $nextMyChildLine = $ast->children[$i + 1]->lineno;
             }
-            $result .= "(" . str_replace("\n", "\n    ", ast_dump($child, $options, $methods)) . ')';
+            $childResult = ast_dump($child, $options, $methods, $methodLines, $nextMyChildLine);
+            if (trim($childResult)) {
+                if (INDENT_OUTPUT) {
+                    $result .= "\n";
+                }
+                $result .= "(" . str_replace("\n", "\n    ", $childResult) . ')';
+            }
         }
+
 
         if ($isMethod) {
             $methods[$methodName] = $result;
+            $methodLines[$methodName] = [$methodStartsAtLine, max($methodEndsAtLine, $nextChildLine)];
         }
 
         return $result;
