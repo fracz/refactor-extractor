fb1ec78a9a595bc1515b89504977302bb0b0b67f
Wojciech Frącz <fraczwojciech@gmail.com>
2017-08-24 13:57:35 +0200

    Building simplified AST

---
+++ Sample.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Wojciech
+ * Date: 2017-08-24
+ * Time: 12:55
+ */
+
+class Sample
+{
+    public function blabla(int $ala, $ola)
+    {
+        $this->name;
+    }
+}
