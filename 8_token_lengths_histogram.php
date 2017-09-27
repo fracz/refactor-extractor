93ea583f41948e65a548b0bd38f799d45722fe2c
Wojciech Frącz <fraczwojciech@gmail.com>
2017-09-27 19:10:53 +0200

    Tokens histograms added

--- 8_token_lengths_histogram.php
+++ 8_token_lengths_histogram.php
@@ -2,27 +2,36 @@
 require 'vendor/autoload.php';
 
 $astDir = __DIR__ . '/input/ast/';
-$input = $astDir . 'deleted.txt';
 
-$rows = explode(PHP_EOL, file_get_contents($input));
+$asts = ['added', 'changed-before', 'changed-after', 'deleted'];
 
-$histogram = [];
+foreach ($asts as $ast) {
+    $input = $astDir . $ast . '.txt';
 
-foreach ($rows as $row) {
-    $tokenCount = substr_count($row, ',') + 1;
-    if (!isset($histogram[$tokenCount])) {
-        $histogram[$tokenCount] = 1;
-    } else {
-        ++$histogram[$tokenCount];
+    $rows = explode(PHP_EOL, file_get_contents($input));
+
+    $histogram = [];
+
+    foreach ($rows as $row) {
+        $tokenCount = substr_count($row, ',') + 1;
+        if (!isset($histogram[$tokenCount])) {
+            $histogram[$tokenCount] = 1;
+        } else {
+            ++$histogram[$tokenCount];
+        }
     }
+
+    ksort($histogram);
+
+    $results = '';
+
+    foreach ($histogram as $tokenCount => $freq) {
+        $results .= $tokenCount . "\t" . $freq . PHP_EOL;
+    }
+
+    file_put_contents($astDir . $ast . '-histogram.txt', $results);
+
+    unset($rows);
+    unset($histogram);
+    unset($results);
 }
-
-ksort($histogram);
-
-$results = '';
-
-foreach ($histogram as $tokenCount => $freq) {
-    $results .= $tokenCount . "\t" . $freq . PHP_EOL;
-}
-
-file_put_contents($astDir . '/histogram.txt', $results);
