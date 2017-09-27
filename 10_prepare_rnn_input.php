93138ecdb8359caa6b2764bde4b52c85ef603337
Wojciech Frącz <fraczwojciech@gmail.com>
2017-09-27 22:03:51 +0200

    ?

---
+++ 10_prepare_rnn_input.php
@@ -0,0 +1,55 @@
+<?php
+require 'vendor/autoload.php';
+
+ini_set('memory_limit', '2G');
+
+$maxLength = 50;
+$astDir = __DIR__ . '/input/ast/';
+
+$asts = ['added', 'changed-before', 'changed-after', 'deleted'];
+
+$readDataset = function ($file, $Y) use ($maxLength, $astDir) {
+    $rows = explode(PHP_EOL, file_get_contents($astDir . $file));
+    $notTooLongRows = array_filter($rows, function ($row) use ($maxLength) {
+        $tokenCount = substr_count($row, ',') + 1;
+        return $tokenCount <= $maxLength;
+    });
+    unset($rows);
+
+    return array_map(function ($row) use ($Y, $maxLength) {
+        $tokens = explode(',', $row);
+        $length = count($tokens);
+        $label = $Y;
+        $padded = array_pad($tokens, $maxLength, 0);
+        return [
+            'X' => implode(',', $padded),
+            'Y' => implode(',', $label),
+            'len' => $length,
+        ];
+    }, $notTooLongRows);
+};
+
+$datasetBefore = $readDataset('changed-before.txt', [0, 1]);
+$datasetAfter = $readDataset('changed-after.txt', [1, 0]);
+
+$dataset = array_merge($datasetBefore, $datasetAfter);
+
+unset($datasetBefore);
+unset($datasetAfter);
+
+shuffle($dataset);
+
+file_put_contents(__DIR__ . '/input/rnn/input.csv', implode(PHP_EOL, array_map(function ($row) {
+    return $row['X'];
+}, $dataset)));
+file_put_contents(__DIR__ . '/input/rnn/labels.csv', implode(PHP_EOL, array_map(function ($row) {
+    return $row['Y'];
+}, $dataset)));
+file_put_contents(__DIR__ . '/input/rnn/lengths.csv', implode(',', array_map(function ($row) {
+    return $row['len'];
+}, $dataset)));
+//    file_put_contents($astDir . $ast . '-max-' . $maxLength . '-lengths.txt', implode(',', $lenghts));
+
+//    unset($padded);
+//    unset($lenghts);
+//    unset($notTooLongRows);
--- 9_filter_out_too_long-asts.php
+++ 9_filter_out_too_long-asts.php
@@ -1,6 +1,8 @@
 <?php
 require 'vendor/autoload.php';
 
+ini_set('memory_limit','1000M');
+
 $maxLength = 2000;
 $astDir = __DIR__ . '/input/ast/';
 
@@ -10,15 +12,29 @@ foreach ($asts as $ast) {
     $input = $astDir . $ast . '.txt';
 
     $rows = explode(PHP_EOL, file_get_contents($input));
-    $notTooLongRows = array_filter($rows, function($row) use ($maxLength) {
+    $notTooLongRows = array_filter($rows, function ($row) use ($maxLength) {
         $tokenCount = substr_count($row, ',') + 1;
         return $tokenCount <= $maxLength;
     });
+    unset($rows);
 
     shuffle($notTooLongRows);
 
-    file_put_contents($astDir . $ast . '-max-' . $maxLength . '.txt', implode(PHP_EOL, $notTooLongRows));
+    $lenghts = array_map(function ($row) {
+        return substr_count($row, ',') + 1;
+    }, $notTooLongRows);
 
-    unset($rows);
+    $padded = array_map(function ($row) use ($maxLength) {
+        $tokens = explode(',', $row);
+        $padded = array_pad($tokens, $maxLength, 0);
+        unset($tokens);
+        return implode(',', $padded);
+    }, $notTooLongRows);
+
+    file_put_contents($astDir . $ast . '-max-' . $maxLength . '.txt', implode(PHP_EOL, $padded));
+    file_put_contents($astDir . $ast . '-max-' . $maxLength . '-lengths.txt', implode(',', $lenghts));
+
+    unset($padded);
+    unset($lenghts);
     unset($notTooLongRows);
 }
