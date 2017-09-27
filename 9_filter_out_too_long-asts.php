6e5533ce89442d54ee342e9988ccbd006d8ecd27
Wojciech Frącz <fraczwojciech@gmail.com>
2017-09-27 19:21:59 +0200

    Filtered too long ASTs

---
+++ 9_filter_out_too_long-asts.php
@@ -0,0 +1,24 @@
+<?php
+require 'vendor/autoload.php';
+
+$maxLength = 2000;
+$astDir = __DIR__ . '/input/ast/';
+
+$asts = ['added', 'changed-before', 'changed-after', 'deleted'];
+
+foreach ($asts as $ast) {
+    $input = $astDir . $ast . '.txt';
+
+    $rows = explode(PHP_EOL, file_get_contents($input));
+    $notTooLongRows = array_filter($rows, function($row) use ($maxLength) {
+        $tokenCount = substr_count($row, ',') + 1;
+        return $tokenCount <= $maxLength;
+    });
+
+    shuffle($notTooLongRows);
+
+    file_put_contents($astDir . $ast . '-max-' . $maxLength . '.txt', $notTooLongRows);
+
+    unset($rows);
+    unset($notTooLongRows);
+}
