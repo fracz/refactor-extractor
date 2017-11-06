d410e2de40b1ce4fbef834c62c7b92859e3ce7d1
Wojciech Frącz <fraczwojciech@gmail.com>
2017-11-06 22:29:14 +0100

    GPU requirements

--- tensorflow/quality.py
+++ tensorflow/quality.py
@@ -4,16 +4,17 @@ import numpy as np
 import csv
 import math
 from random import shuffle
+import sys
 
 ####################### PARAMS
 vocabulary_size = 129 # liczba tokenow w kodzie
 embedding_size = 100 # rozmiar wektora wejsciowego (arbitralny chyba?)
 num_hidden = 128
 num_classes = 2
-training_steps = 30
-batch_size = 32 #128
-display_step = 10#200
-restore = True
+training_steps = 100000
+batch_size = 64 #128
+display_step = 200
+restore = False
 
 
 ################# DATA INPUT
@@ -26,6 +27,7 @@ class RefactorDataset():
         self.max_seqlen = len(self.data[0])
         self.test_len = math.floor(len(self.data) * .15)
         self.batch_id = self.test_len
+        print("Input size: " + str(self.max_seqlen) + 'x' + str(len(self.data)))
 
     def validation(self, batch_size):
         return self.data[0:batch_size], self.labels[0:batch_size], self.seqlen[0:batch_size]
@@ -47,47 +49,56 @@ dataset = RefactorDataset()
 
 ############################################ RNN
 
-train_inputs = tf.placeholder(tf.int32, shape=[None, dataset.max_seqlen])
-train_outputs = tf.placeholder(tf.float32, shape=[None, num_classes])
+device_name = sys.argv[1]
 
-# https://github.com/aymericdamien/TensorFlow-Examples/blob/master/examples/3_NeuralNetworks/dynamic_rnn.py
-seqlen = tf.placeholder(tf.int32, [None])
+with tf.device("/" + device_name):
 
-# https://www.tensorflow.org/tutorials/word2vec#building_the_graph
-embeddings = tf.Variable(tf.random_uniform([vocabulary_size, embedding_size], -1.0, 1.0))
+    train_inputs = tf.placeholder(tf.int32, shape=[None, dataset.max_seqlen])
+    train_outputs = tf.placeholder(tf.float32, shape=[None, num_classes])
 
-embed = tf.nn.embedding_lookup(embeddings, train_inputs)
+    # https://github.com/aymericdamien/TensorFlow-Examples/blob/master/examples/3_NeuralNetworks/dynamic_rnn.py
+    seqlen = tf.placeholder(tf.int32, [None])
 
-weights = {
-    # Hidden layer weights => 2*n_hidden because of forward + backward cells
-    'out': tf.Variable(tf.random_normal([2*num_hidden, num_classes]))
-}
-biases = {
-    'out': tf.Variable(tf.random_normal([num_classes]))
-}
+    # https://www.tensorflow.org/tutorials/word2vec#building_the_graph
+    embeddings = tf.Variable(tf.random_uniform([vocabulary_size, embedding_size], -1.0, 1.0))
 
-def BiRNN(x, seqlen, weights, biases):
-    inputs = tf.unstack(x, num=dataset.max_seqlen, axis=1)
-    lstm_fw_cell = rnn.BasicLSTMCell(num_hidden, forget_bias=1.0)
-    lstm_bw_cell = rnn.BasicLSTMCell(num_hidden, forget_bias=1.0)
-    outputs, _, _ = rnn.static_bidirectional_rnn(lstm_fw_cell, lstm_bw_cell, inputs, dtype=tf.float32, sequence_length=seqlen)
-    return tf.matmul(outputs[-1], weights['out']) + biases['out']
+    embed = tf.nn.embedding_lookup(embeddings, train_inputs)
 
-logits = BiRNN(embed, seqlen, weights, biases)
-prediction = tf.nn.softmax(logits)
+    weights = {
+        # Hidden layer weights => 2*n_hidden because of forward + backward cells
+        'out': tf.Variable(tf.random_normal([2*num_hidden, num_classes]))
+    }
+    biases = {
+        'out': tf.Variable(tf.random_normal([num_classes]))
+    }
 
-loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=train_outputs))
-#optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
-optimizer = tf.train.AdamOptimizer(learning_rate=1e-4)
-train_op = optimizer.minimize(loss_op)
+    def BiRNN(x, seqlen, weights, biases):
+        inputs = tf.unstack(x, num=dataset.max_seqlen, axis=1)
+        lstm_fw_cell = rnn.BasicLSTMCell(num_hidden, forget_bias=1.0)
+        lstm_bw_cell = rnn.BasicLSTMCell(num_hidden, forget_bias=1.0)
+        outputs, _, _ = rnn.static_bidirectional_rnn(lstm_fw_cell, lstm_bw_cell, inputs, dtype=tf.float32, sequence_length=seqlen)
+        return tf.matmul(outputs[-1], weights['out']) + biases['out']
 
-correct_pred = tf.equal(tf.argmax(prediction, 1), tf.argmax(train_outputs, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
-init = tf.global_variables_initializer()
+    logits = BiRNN(embed, seqlen, weights, biases)
+    prediction = tf.nn.softmax(logits)
 
-saver = tf.train.Saver()
+    loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=train_outputs))
+    #optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+    optimizer = tf.train.AdamOptimizer(learning_rate=1e-4)
+    train_op = optimizer.minimize(loss_op)
 
-with tf.Session() as sess:
+    correct_pred = tf.equal(tf.argmax(prediction, 1), tf.argmax(train_outputs, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+    init = tf.global_variables_initializer()
+
+    saver = tf.train.Saver()
+    
+config = tf.ConfigProto(
+    device_count = {'GPU': 1}
+#    , log_device_placement=True
+)
+
+with tf.Session(config=config) as sess:
     if restore:
         new_saver = tf.train.import_meta_graph('./trained/model.meta')
         new_saver.restore(sess, tf.train.latest_checkpoint('./trained'))
