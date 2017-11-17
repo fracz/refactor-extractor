96b44703d9419a8225ea08ac623a5673017f8c89
Wojciech Frącz <fraczwojciech@gmail.com>
2017-11-17 23:43:58 +0100

    Java ASTs

--- java-parser/src/main/java/MethodTokenizer.java
+++ java-parser/src/main/java/MethodTokenizer.java
@@ -2,32 +2,41 @@ import com.github.javaparser.JavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.expr.*;
 import com.github.javaparser.ast.visitor.VoidVisitor;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 import com.github.javaparser.utils.Pair;
+import com.google.common.collect.ImmutableSet;
+import org.apache.commons.lang3.StringUtils;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class MethodTokenizer {
 
     public static Set<String> ALL_TOKENS = new HashSet<>();
 
-    public static void main(String[] args) throws Exception {
-        System.out.println(tokenize("src/main/java/MethodTokenizer.java"));
-    }
+    private static final Set<String> SIMPLE_NODES = ImmutableSet.of(
+            SimpleName.class.getSimpleName(),
+            NameExpr.class.getSimpleName(),
+            StringLiteralExpr.class.getSimpleName(),
+            NullLiteralExpr.class.getSimpleName(),
+            Name.class.getSimpleName()
+    );
 
-    public static List<Pair<MethodDeclaration, String>> tokenize(String filePath) {
+    public static Map<String, Pair<MethodDeclaration, String>> tokenize(String filePath) {
         return new MethodTokenizer().doTokenize(filePath);
     }
 
-    private List<Pair<MethodDeclaration, String>> methods = new ArrayList<>();
+    private Map<String, Pair<MethodDeclaration, String>> methods = new HashMap<>();
 
-    private List<Pair<MethodDeclaration, String>> doTokenize(String filePath) {
+    private Set<String> overloadedMethods = new HashSet<>();
+
+    private Map<String, Pair<MethodDeclaration, String>> doTokenize(String filePath) {
         // creates an input stream for the file to be parsed
         FileInputStream in = null;
         try {
@@ -42,6 +51,8 @@ public class MethodTokenizer {
         // visit and print the methods names
         cu.accept(new MethodVisitor(), null);
 
+        this.overloadedMethods.forEach(this.methods::remove);
+
         return this.methods;
     }
 
@@ -53,17 +64,28 @@ public class MethodTokenizer {
         public void visit(MethodDeclaration method, Void arg) {
             final StringBuilder ast = new StringBuilder();
 
+            if (methods.containsKey(method.getNameAsString())) {
+                overloadedMethods.add(method.getNameAsString());
+                return;
+            }
+
             VoidVisitor<Integer> v = new TreeVisitior() {
                 @Override
                 public void in(Node n, Integer indentLevel) {
                     String token = n.getClass().getSimpleName();
                     ALL_TOKENS.add(token);
-                    ast.append("(" + token);
+                    if (SIMPLE_NODES.contains(token)) {
+                        ast.append(StringUtils.repeat('\t', indentLevel) + token + '\n');
+                    } else {
+                        ast.append(StringUtils.repeat('\t', indentLevel) + "(" + token + '\n');
+                    }
                 }
 
                 @Override
                 public void out(Node n, Integer indentLevel) {
-                    ast.append(")");
+                    if (!SIMPLE_NODES.contains(n.getClass().getSimpleName())) {
+                        ast.append(StringUtils.repeat('\t', indentLevel) + ")" + '\n');
+                    }
                 }
             };
 
@@ -71,7 +93,7 @@ public class MethodTokenizer {
 
             String buildAst = ast.toString();//.replaceAll("\\(([^\\)]+)\\)", "$1");
 
-            methods.add(new Pair<>(method, buildAst));
+            methods.put(method.getNameAsString(), new Pair<>(method, buildAst));
 
             super.visit(method, arg);
         }
